c4077e3e05c653be6666333ec9cfa41fa3cb21d0
Pabllo Lins <pablloricardolins@gmail.com>
2021-08-26 18:45:06 -0300

    Codigo 7 - Aula_39_LOW_POWER_LED

---
+++ Codes/7_Aula_39_LOW_POWER_LED/7_Aula_39_LOW_POWER_LED.asm
@@ -0,0 +1,37 @@
+;--------------------------------------------------------
+;Programer: Pabllo Lins
+;Date: 20/08/2021(dd/mm/yyyy)
+;Code: 39 - PISCANDO UM LED COM BAIXO CONSUMO
+;MCU: MSP430FG4618
+
+;MSP430FG4618F/2013 Experimenter Board	(SIMULATOR)	
+;-------------------------------------------------------------------------------
+
+
+#include <msp430xG46x.h>
+;-------------------------------------------------------------------------------
+		RSEG CSTACK ; Define stack segment
+;-------------------------------------------------------------------------------
+		RSEG CODE ; Assemble to Flash memory
+;-----------------------------------------------------------------------------
+RESET 		mov.w #SFE(CSTACK),SP ; Initialize stackpointer
+StopWDT 	mov.w #WDTPW+WDTHOLD,&WDTCTL ; Stop WDT
+SetupFLL 	bis.b #XCAP14PF,&FLL_CTL0 ; Configure load caps
+		bis.b #BIT1,&P5DIR ; Set P5.1 as Output
+SetupBT 	mov.b #BTDIV+BT_fCLK2_DIV16, & BTCTL ; ACLK/(256*16)
+		bis.b #BTIE,&IE2 ; Enable BT interrupt
+Mainloop 	bis.w #LPM3+GIE,SR ; Enter LPM3, enable interrupts
+		nop ; Required for Debugger
+;------------------------------------------------------------------------------
+Basic_Timer_ISR ;// Basic Timer Interrupt Service Routine
+;------------------------------------------------------------------------------
+		xor.b #BIT1,&P5OUT ; Toggle P5.1 (LED)
+		reti
+;------------------------------------------------------------------------------
+		COMMON INTVEC ; Interrupt Vectors
+;------------------------------------------------------------------------------
+		ORG RESET_VECTOR ; MSP430 RESET Vector
+		DW RESET ;
+		ORG BASICTIMER_VECTOR ; MSP430 Basic Timer Interrupt Vector
+		DW Basic_Timer_ISR
+		END
